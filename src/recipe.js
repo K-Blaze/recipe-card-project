3daf387a857aa71581a8ce2e07e5c6fd76ae5625
Kenyada Peoples <beautyuniversity1986@gmail.com>
2026-04-13 08:43:41 -0500

    Create recipe.js
    /*
    recipe.js – Recipe Data File
    
    This file stores all of the recipe data used in the application. It contains an array of recipe objects, where each object includes an id, name, cook time, image, and a list of ingredients.
    
    This data is exported and used in App.js, where it is looped through using the .map() method to display multiple recipe cards on the screen.
    
    Keeping the data in a separate file helps keep the project organized and makes it easier to update or add new recipes without changing the UI code.
    */

---
+++ src/recipe.js
@@ -0,0 +1,28 @@
+const recipes = [
+  {
+    id: 1,
+    name: "Spaghetti",
+    cookTime: "25 mins",
+    image:
+      "https://images.unsplash.com/photo-1622973536968-3ead9e780960?auto=format&fit=crop&w=500&q=80",
+    ingredients: ["Pasta", "Tomato Sauce", "Garlic", "Parmesan"]
+  },
+  {
+    id: 2,
+    name: "Chicken Salad",
+    cookTime: "15 mins",
+    image:
+      "https://images.unsplash.com/photo-1546793665-c74683f339c1?auto=format&fit=crop&w=500&q=80",
+    ingredients: ["Chicken", "Lettuce", "Tomatoes", "Cucumber"]
+  },
+  {
+    id: 3,
+    name: "Pancakes",
+    cookTime: "20 mins",
+    image:
+      "https://images.unsplash.com/photo-1528207776546-365bb710ee93?auto=format&fit=crop&w=500&q=80",
+    ingredients: ["Flour", "Milk", "Eggs", "Syrup"]
+  }
+];
+
+export default recipes;
